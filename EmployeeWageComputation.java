c4f4168dce4144950cb81065d667ebfdeff60549
unknown <sidhodhank2018@gmail.com>
2022-09-10 10:52:07 +0530

    Calculating Wages For Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,16 +1,10 @@
 package Basics;
 
 public class employeeWageComputation {
-<<<<<<< HEAD
-	public static final int IS_PART_TIME = 1;
-  	 public static final int IS_FULL_TIME = 2;
-  	 public static final int EMP_RATE_PER_HOUR = 20;
-=======
 	 public static final int IS_PART_TIME = 1;
 	 public static final int IS_FULL_TIME = 2;
 	 public static final int EMP_RATE_PER_HOUR = 20;
 	 public static final int NUM_OF_WORKING_DAYS = 20;
->>>>>>> UC-5_Calculating_Wages_For_Month
 	 public static void main( String[] args )
 	    {
 	        System.out.println(""
@@ -20,8 +14,6 @@ public class employeeWageComputation {
 	                        + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
 	                        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
 	                        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
-<<<<<<< HEAD
-	   		 int empHrs = 0;
 	   		 int empWage = 0;
 	   		 int empCheck = (int) Math.floor(Math.random() * 10)% 3;
 	   		 switch(empCheck) {
