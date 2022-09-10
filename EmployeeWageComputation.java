cee4707088ec7aadb2b7b1edc4f3274bb538d0dc
unknown <sidhodhank2018@gmail.com>
2022-09-10 09:52:44 +0530

    Check Employee Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,26 @@
-System.out.println(""
-    			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
-    			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
-    			+ "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
-    			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
-    			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
-    			+ "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
+package com.day3_program.Basics;
+public class UC_1_Check_Employee_Attendance 
+{
+    public static void main( String[] args )
+    {
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+	System.out.println(""
+                        + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
+                        + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
+                        + "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
+                        + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
+                        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
+                        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
+
+        if (empCheck == IS_FULL_TIME)
+        {
+        System.out.println("Employee is present");
+        }
+        else 
+        {
+        	System.out.println("Employee is Absent");
+        }
+        
+    }
+}
