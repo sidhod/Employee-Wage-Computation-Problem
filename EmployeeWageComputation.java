ab8ae7151c61be5f783ac9aca9e167079da5419a
unknown <sidhodhank2018@gmail.com>
2022-09-10 10:13:44 +0530

    Calculating Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,8 +3,6 @@ package Basics;
 public class employeeWageComputation {
 	 public static void main( String[] args )
 	    {
-	        int IS_FULL_TIME = 1;
-	        double empCheck = Math.floor(Math.random()*10)%2;
 	        System.out.println(""
 	                        + "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
 	                        + " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
@@ -13,14 +11,17 @@ public class employeeWageComputation {
 	                        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
 	                        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
 
-	        if (empCheck == IS_FULL_TIME)
-	        {
-	        System.out.println("Employee is present");
-	        }
-	        else
-	        {
-	                System.out.println("Employee is Absent");
-	        }
+	        int IS_FULL_TIME =1;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
+			int empWage = 0;
+			double empCheck = Math.floor(Math.random()*10)%2;
+			if (empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: "+ empWage);
 
 	    }
 
