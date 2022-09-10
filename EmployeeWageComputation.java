719fe053af581aeddd1a6597bdafbedc08c95bab
unknown <sidhodhank2018@gmail.com>
2022-09-10 10:42:01 +0530

    Calculating Wages For Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,6 +1,10 @@
 package Basics;
 
 public class employeeWageComputation {
+	 public static final int IS_PART_TIME = 1;
+	 public static final int IS_FULL_TIME = 2;
+	 public static final int EMP_RATE_PER_HOUR = 20;
+	 public static final int NUM_OF_WORKING_DAYS = 20;
 	 public static void main( String[] args )
 	    {
 	        System.out.println(""
@@ -10,22 +14,24 @@ public class employeeWageComputation {
 	                        + " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
 	                        + "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
 	                        + "--------------- EMPLOYEE WAGE COMPUTATION-----------------------\r\n");
-
-	        int IS_FULL_TIME = 1;
-			int IS_PART_TIME = 2;
-			int EMP_RATE_PER_HOUR = 20;
-			int empHrs = 0;
-			int empWage = 0;
-			double empCheck = Math.floor(Math.random()*10)%3;
-			if (empCheck == IS_PART_TIME)
-				empHrs = 4;
-			else if (empCheck == IS_FULL_TIME)
-				empHrs = 8;
-			else
-				empHrs = 0;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage: "+ empWage);
-
+	        int empWage=0, empHrs=0, totalEmployeewage=0;
+			 for(int day=0; day<NUM_OF_WORKING_DAYS; day++) {
+				 int empCheck = (int) Math.floor(Math.random() * 10)% 3;
+				 switch(empCheck) {
+				    case IS_PART_TIME:
+				    empHrs = 4;
+				    break;
+				    case IS_FULL_TIME:
+					    empHrs = 8;
+					    break;
+					default:
+						empHrs=0;
+				 }
+				 empWage = empHrs * EMP_RATE_PER_HOUR;
+				 totalEmployeewage += empWage;
+			 }
+			 System.out.println("Total Employee Wage is: " + totalEmployeewage);
+	   	 
 	    }
 
 
